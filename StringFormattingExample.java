271e28125ca7828b4de6c3f0ffa6fb6eaefbdd68
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-11-07 20:03:28 +0530

    StringFormattingExample.java

---
+++ StringFormattingExample.java
@@ -0,0 +1,25 @@
+class StringFormattingExample {
+  public static void main(String[] arguments) {
+    String normalstring = "hi"; //literal string
+    String normalstring2 = "hi";
+
+    String objectstring1 = new String("meow"); // object string
+    String objectstring2 = new String("meow");
+
+    System.out.println(normalstring == normalstring2);
+    System.out.println(objectstring1 == objectstring2);
+
+    // EXAMPLE OF FORMAT SPECIFIES
+    String text = "Example";
+    int year = 2024;
+    Float floatingValue = 1.5f;
+
+    String Combined_string = String.format("This is an %s. I am from year %d. My car is %f years old",text, year, floatingValue);
+
+   System.out.println(Combined_string);
+      
+
+    
+    
+  }
+}
