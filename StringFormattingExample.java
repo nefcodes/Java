5da4f4b361f18d9c925d20f8f139989fb6d5c6de
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-11-07 20:15:33 +0530

    StringFormattingExample.java

--- StringFormattingExample.java
+++ StringFormattingExample.java
@@ -6,12 +6,19 @@ class StringFormattingExample {
     String objectstring1 = new String("meow"); // object string
     String objectstring2 = new String("meow");
 
+    //check if string content is the same value.
     System.out.println(normalstring == normalstring2);
+    // check if the objects are the same
     System.out.println(objectstring1 == objectstring2);
+
+    // this will check the value of both the objects and return booleam
+    System.out.println(objectstring1.equals(objectstring2));
+
+
 
     // EXAMPLE OF FORMAT SPECIFIES
 // FORMAT SPECIFIES FOR, String %s, Double/Float %f, char %c, boolean %b, int %d
-    
+
     String text = "Example";
     int year = 2024;
     Float floatingValue = 1.5f;
@@ -19,9 +26,9 @@ class StringFormattingExample {
     String Combined_string = String.format("This is an %s. I am from year %d. My car is %f years old",text, year, floatingValue);
 
    System.out.println(Combined_string);
-      
 
-    
-    
+
+
+
   }
 }
