ba473785d43eb8a7adfc43ffebb8cb59eabc4a1c
nefcodes <168460285+nefcodes@users.noreply.github.com>
2025-01-22 00:00:02 +0530

    Create MathFunctions.java

---
+++ MicroProjects/MathFunctions.java
@@ -0,0 +1,20 @@
+
+
+public class main {
+        public static void main (String[] args) {
+                // Math functions
+
+
+                System.out.println("2 raise to 3: " + Math.pow(2, 3) );
+                System.out.println( "Square root: "+ Math.sqrt(8) );
+                System.out.println( "Absolute value: " + Math.abs(-0.33) );
+                System.out.println( "Round off: " + Math.round(0.87) );
+                System.out.println( "Ceiling: " + Math.ceil(-3.14) );
+                System.out.println( "Floor down: " + Math.floor(2.3) );
+                System.out.println( "Max value: " + Math.max(-0.33, 100) );
+                System.out.println( "Min value: " + Math.min(-0.33, 100) );
+
+
+
+        }
+}
