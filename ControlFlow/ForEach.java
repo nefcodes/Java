804d4ddd7fb255c188799a1fbb20cb65af4129db
nefcodes <168460285+nefcodes@users.noreply.github.com>
2025-01-30 23:23:03 +0530

    Create ForEach.java

---
+++ ControlFlow/ForEach.java
@@ -0,0 +1,20 @@
+public class main {
+
+    public static void main(String[] args) {
+        // for each loop
+        String[] animals = {"cat", "dog", "elephant", "Rhino"};
+
+            int counter = 0;
+
+
+
+            for (String x : animals) {
+                System.out.println("The " + counter++ + " index animal is " + x);
+            };
+
+
+
+
+    }
+
+}
