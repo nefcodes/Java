f7d91a5b346485de2efc2f488e04bbe66099b430
nefcodes <168460285+nefcodes@users.noreply.github.com>
2025-06-25 18:56:40 +0530

    Update 1. Find if a number repeats in an array.java

--- DataStructures/Neetcode150/1. Find if a number repeats in an array.java
+++ DataStructures/Neetcode150/1. Find if a number repeats in an array.java
@@ -1,2 +1,5 @@
-//  If you have an unsorted array, costs Big O (n square)
+//  If you have an unsorted array, finding duplicate costs Big O (n square)
 //  with a sorted array, repeating items are next to each other! costs, Big O (n Log n)
+
+// but if you use a hashset (can't contain repeating items)
+// it takes big (n) and space big (n)
