ac1b3f19c139d6afd1cc20eaa12ea421a07211da
nefcodes <168460285+nefcodes@users.noreply.github.com>
2025-07-07 22:08:16 +0530

    Create 5. Maximum Sub array sum.java

---
+++ DataStructures/Neetcode150/5. Maximum Sub array sum.java
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+a sequence [a1, a2, a3, a4....]
+
+return the sum of connected sub array.
+if at any point sum becomes zero, discard it. bcz even if there is +ve value. -ve value came in between and cuts the substring
+so need to discard it.
+
+complexity o(n)
+ */
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxYet = nums[0]; // set to first item
+        int currentSum = nums[0];
+
+        // iterating from 1, bcz we already covered 0th element
+        for (int i = 1; i < nums.length; i++){
+ 
+            //house keeping
+            if(currentSum < 0){
+                currentSum = 0; // if first element neg, make zero
+            }
+
+            currentSum = currentSum + nums[i]; // in every iteration add values sumed yet + current index
+
+            if(currentSum > maxYet) {
+                maxYet = currentSum;
+            }
+        }
+        return maxYet;
+
+    }
+}
