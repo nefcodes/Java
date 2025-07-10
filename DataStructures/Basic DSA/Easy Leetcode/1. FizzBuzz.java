0e46ecde572166e6c2d2452e33c4d2b44ada652b
nefcodes <168460285+nefcodes@users.noreply.github.com>
2025-07-10 21:22:31 +0530

    Create 1. FizzBuzz.java

---
+++ DataStructures/Basic DSA/Easy Leetcode/1. FizzBuzz.java
@@ -0,0 +1,28 @@
+/*
+
+FIZZ BUZZ
+given integer n, write a string which has n items
+if div by 3 = Fizz, if div by 5 = Buzz, if both 3 and 5 = FizzBuzz, otherwise just the number
+ */
+
+import java.util.ArrayList;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+    List<String> answer = new ArrayList<>();
+
+    for(int cur=1; cur <= n; cur++){ // not using it from 0, bcz we want number from 1
+        if(cur%3 == 0 && cur % 5 ==0){
+            answer.add("FizzBuzz");
+        } else if( cur %3 ==0){
+            answer.add("Fizz");
+        } else if (cur %5 ==0) {
+            answer.add("Buzz");
+        } else{
+            answer.add(cur + ""); // int to string
+        }
+
+    }
+    return answer;
+    }
+}
